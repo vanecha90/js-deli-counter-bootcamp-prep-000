b795526c7091da750390ff8bda84d6519345b02d
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-04-09 16:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(katzDeliLine,newname){
   var place = katzDeliLine.length+1
   katzDeliLine.push(newname)
   
-  return "Welcome, " + newname +". You are number"       + place +  "in line.";
+  return "Welcome, " + newname +". You are number "       + place +" in line.";
  
 }
 
