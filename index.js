98fe557b1a311f29c4192e9420fef1912fb2428e
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-04-09 18:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ function currentLine(katzDeliLine){
   
   var line = 'The line is currently: 1. ${katzDeliLine[0]}'
  for(let i = 1; i< katzDeliLine.length; i++  ){
-   return
+   return "The line is currently:{line}
+   
+   
+   
  }
 }
