48374015bc62f25b8f67f50730b418ea0df10902
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-04-09 18:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function currentLine(katzDeliLine){
   
  for(let i = 1; i< katzDeliLine.length; i++){
   
-  line += `, ${i+1}. ${katzDeliLine[i]}`
+  line += ", ${i+1}. ${katzDeliLine[i]}"
    
  }
- return line;
+ return line
 }
